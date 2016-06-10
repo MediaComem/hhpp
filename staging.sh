9a0a653d417ddc3cc1e100d6389cf958e1bbafed
Simon Oulevay <simon.oulevay@gmail.com>
2016-06-10 15:09:01 +0200

    Staging script: print amazon bucket & region.

--- staging.sh
+++ staging.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 jekyll build
+
+echo "Bucket: $AWS_BUCKET"
+echo "Region: $AWS_REGION"
+
 s3cmd -v --region "${AWS_REGION}" --no-preserve --no-mime-magic --guess-mime-type sync _site/ "s3://${AWS_BUCKET}"
