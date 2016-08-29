dae755a4d246e3562ec30831d6e14b350df1b724
Simon Oulevay <simon.oulevay@gmail.com>
2016-08-29 13:28:18 +0200

    Production script to build and push site to github pages.

---
+++ production.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+set -e
+
+REPO_URL="$(git remote get-url origin)"
+CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+CURRENT_COMMIT="$(git rev-parse --verify --short HEAD)"
+
+echo
+
+
+
+echo "Creating temporary directory..."
+
+TMP_DIR="$(mktemp -d -t hhpp)"
+
+function cleanup {
+  echo "Cleaning up..."
+  #rm -fr "$TMP_DIR"
+}
+
+trap cleanup EXIT
+
+echo "$TMP_DIR"
+echo
+
+
+
+echo "Building site..."
+echo
+jekyll build -d "$TMP_DIR"
+echo
+
+
+
+echo "Fetching repository..."
+echo
+cd "$TMP_DIR"
+git init
+git remote add -t gh-pages origin "$REPO_URL"
+git fetch
+echo
+
+
+
+echo "Committing & pushing changes..."
+echo
+cd "$TMP_DIR"
+git reset --soft origin/gh-pages
+git add --all .
+git commit -m "Generated Jekyll site from ${CURRENT_BRANCH}@${CURRENT_COMMIT}"
+git push origin master:gh-pages
+echo
+
+echo "Done!"
